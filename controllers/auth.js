4f889c57bdc8aad437b73106214b2bb7b146292f
Pedro Fernandez <pand4designers@gmail.com>
2023-03-21 17:03:06 -0500

    main

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,21 @@
 import { User } from "../models/index.js";
 
 function register(req, res) {
-	console.log(req.body);
-	res.status(201).send({ msg: "Todo OK" });
+	const { email, passwordUser } = req.body;
+
+	const user = new User({
+		email: email.toLowerCase(),
+		passwordUser: passwordUser,
+	});
+
+	user.save((error, userStorage) => {
+		if (error) {
+			console.log(error);
+			res.status(400).send({ msg: "Error al registrar el usuario" });
+		} else {
+			res.status(201).send(userStorage);
+		}
+	});
 }
 
 export const AuthController = {
